195efedfb23618998092f9d620ab0e4b292a9934
sieugene <sieugene@mail.ru>
2024-01-11 19:02:00 +0900

    feat : 学生を作成、基本の情報を表示

--- src/entities/student/ui/Student/index.tsx
+++ src/entities/student/ui/Student/index.tsx
@@ -1,8 +1,45 @@
+import { SUPABASE_CLIENT } from "shared/api/supabase";
 import { useSessionContext } from "shared/providers";
+import useSWR from "swr";
 
 export const Student = () => {
-  const { session } = useSessionContext();
+  const { user } = useSessionContext();
+  const { data, isLoading } = useSWR(
+    user && `/student/${user?.id}`,
+    async () => {
+      if (!user) return null;
+      const { data: students, error } = await SUPABASE_CLIENT.from("students")
+        .select("*")
+        .eq("userId", user.id)
+        .single();
+      if (error) {
+        throw new Error(error.message);
+      }
+      return students;
+    }
+  );
 
-  
-  return <div></div>;
+  const joinAsStudent = async () => {
+    if (!user) return;
+    await SUPABASE_CLIENT.from("students")
+      .insert([{ userId: user?.id }])
+      .select();
+  };
+
+  if (isLoading) {
+    return <h2>Loading...</h2>;
+  }
+
+  return (
+    <div>
+      {!data?.id ? (
+        <>
+          <h2>You're not a student</h2>
+          <button onClick={joinAsStudent}>Join as Student</button>
+        </>
+      ) : (
+        <h2>Student id - {data.id}</h2>
+      )}
+    </div>
+  );
 };
